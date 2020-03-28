0debb52059be3c58ce68c1173cf3dbf546979c32
nmrnm <noahrmohabbat@gmail.com>
2020-03-28 08:29:03 -0500

    fixed exercise4 because I slightly misunderstood requirements

--- exercise4/index.js
+++ exercise4/index.js
@@ -4,11 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   submit.addEventListener("click", () => {
     let para = document.querySelector("#para");
-    para.style.backgroundColor = document.querySelector("#backgroundColor").value;
-    let stringBorder = "" + document.querySelector("#borderWidth").value + "px solid " + document.querySelector("#borderColor").value;
-    para.style.border = stringBorder;
-
-    console.log(typeof(stringBorder), stringBorder)
+    let stringBorder = document.querySelector("#borderWidth").value.toString();
+   
+    let borderR = parseInt(document.querySelector("#borderR").value);
+    let borderG = parseInt(document.querySelector("#borderG").value);
+    let borderB = parseInt(document.querySelector("#borderB").value);	  
+    if(stringBorder != ""&& !isNaN(borderR) && !isNaN(borderG) && !isNaN(borderB)){
+        para.style.borderStyle = "solid";
+        para.style.borderWidth = stringBorder;
+	para.style.borderColor = "rgb("+borderR+","+borderG+","+borderB+")";
+    }
+    let backR = parseInt(document.querySelector("#backR").value);
+    let backG = parseInt(document.querySelector("#backG").value);
+    let backB = parseInt(document.querySelector("#backB").value);	     
+    para.style.backgroundColor = "rgb("+backR+","+backG+","+backB+")";
+    console.log(typeof(stringBorder),typeof(backR), backR, stringBorder,borderR, borderG, borderB)
   })
 
 });
